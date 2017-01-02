26d2ade73a5e9a4decebfdcf0c106acd27c8d8ac
michnik <michnik13@gmail.com>
2017-01-02 19:59:13 +0100

    add button

---
+++ app/src/main/java/pl/matczakonline/zaliczeniowa/FloatingActionButton.java
@@ -0,0 +1,57 @@
+package pl.matczakonline.zaliczeniowa;
+
+import android.content.Context;
+import android.graphics.Outline;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.FrameLayout;
+
+/**
+ * A Floating Action Button is a {@link android.widget.Checkable} view distinguished by a circled
+ * icon floating above the UI, with special motion behaviors.
+ */
+public class FloatingActionButton extends FrameLayout {
+
+    public FloatingActionButton(Context context) {
+        this(context, null, 0, 0);
+    }
+
+    public FloatingActionButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0, 0);
+    }
+
+    public FloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public FloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr,
+                                int defStyleRes) {
+        super(context, attrs, defStyleAttr);
+
+        setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setOval(0, 0, getWidth(), getHeight());
+            }
+        });
+
+        setClickable(true);
+
+        setClipToOutline(true);
+
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        return super.onCreateDrawableState(extraSpace + 1);
+    }
+}
--- app/src/main/java/pl/matczakonline/zaliczeniowa/TodoAppActivity.java
+++ app/src/main/java/pl/matczakonline/zaliczeniowa/TodoAppActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by michn on 29.12.2016.
@@ -16,13 +18,28 @@ public class TodoAppActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.todo_app);
         this.initToolbar();
+        this.initAddButton();
 
+
+    }
+
+    private void initAddButton() {
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                test();
+            }
+        });
     }
 
     private void initToolbar() {
         Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
         mActionBarToolbar.setTitle("Testy");
         mActionBarToolbar.setTitleTextColor(Color.WHITE);
+    }
+
+    public static void test() {
+        Log.d("tag", "here we are from todo");
     }
 }
 
